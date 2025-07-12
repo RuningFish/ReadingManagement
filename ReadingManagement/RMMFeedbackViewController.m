e9a5cb7b9a6c01336339207489439dc6617b1152
runingfish <1826931541@qq.com>
2025-07-12 18:29:17 +0800

    设置&意见反馈

--- ReadingManagement/RMMFeedbackViewController.m
+++ ReadingManagement/RMMFeedbackViewController.m
@@ -15,17 +15,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.title = @"意见反馈";
+    self.view.backgroundColor = MAIN_VIEW_COLOR;
+    
+    UILabel *rmm_feedback_title_label = [[UILabel alloc] initWithFrame:CGRectMake(10, 110, self.view.frame.size.width-20, 40)];
+    [self.view addSubview:rmm_feedback_title_label];
+    rmm_feedback_title_label.text = @"请将您的意见或建议填入下方，以便我们改进";
+    rmm_feedback_title_label.textColor = [UIColor blackColor];
+    rmm_feedback_title_label.textAlignment = NSTextAlignmentLeft;
+    rmm_feedback_title_label.font = [UIFont systemFontOfSize:16];
+    
+    UISegmentedControl *rmm_seg_control = [[UISegmentedControl alloc] initWithItems:@[@"建议", @"问题", @"表扬", @"其它",]];
+    [self.view addSubview:rmm_seg_control];
+    rmm_seg_control.frame = CGRectMake(20, 160, self.view.frame.size.width - 40, 40);
+    rmm_seg_control.selectedSegmentIndex = 0;
+    rmm_seg_control.selectedSegmentTintColor = [UIColor systemBlueColor];
+    
+    UITextView *rmm_feedback_textView = [[UITextView alloc] initWithFrame:CGRectMake(10, CGRectGetMaxY(rmm_seg_control.frame) + 35, self.view.frame.size.width - 20, 133)];
+    [self.view addSubview:rmm_feedback_textView];
+    rmm_feedback_textView.layer.borderWidth = 1;
+    rmm_feedback_textView.layer.borderColor = [UIColor colorWithRed:arc4random_uniform(256)/255.0 green:arc4random_uniform(256)/255.0 blue:arc4random_uniform(256)/255.0 alpha:1].CGColor;
+    rmm_feedback_textView.layer.cornerRadius = 5;
+    rmm_feedback_textView.layer.masksToBounds = YES;
+    
+    UIButton *rmm_feedback_confirmButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.view addSubview:rmm_feedback_confirmButton];
+    rmm_feedback_confirmButton.frame = CGRectMake(10, CGRectGetMaxY(rmm_feedback_textView.frame) + 33, [UIScreen mainScreen].bounds.size.width-20, 44);
+    [rmm_feedback_confirmButton setTitle:@"提交" forState:UIControlStateNormal];
+    [rmm_feedback_confirmButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    rmm_feedback_confirmButton.titleLabel.font = [UIFont systemFontOfSize:20];
+    [rmm_feedback_confirmButton addTarget:self action:@selector(rmm_feedback_confirmButtonClick:) forControlEvents:UIControlEventTouchUpInside];
+    rmm_feedback_confirmButton.backgroundColor = [UIColor systemBlueColor];
+    rmm_feedback_confirmButton.layer.cornerRadius = 10;
+    rmm_feedback_confirmButton.layer.masksToBounds = YES;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)rmm_feedback_confirmButtonClick:(UIButton *)button{
+    [self.navigationController popViewControllerAnimated:YES];
 }
-*/
-
 @end
--- ReadingManagement/RMMSettingViewController.m
+++ ReadingManagement/RMMSettingViewController.m
@@ -6,26 +6,80 @@
 //
 
 #import "RMMSettingViewController.h"
-
-@interface RMMSettingViewController ()
-
+#import "RMMFeedbackViewController.h"
+@interface RMMSettingViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (nonatomic, strong) UITableView *rmm_setting_tableView;
+@property (nonatomic, strong) NSArray *rmm_setting_dataSource;
 @end
 
 @implementation RMMSettingViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self.view addSubview:self.rmm_setting_tableView];
+    self.rmm_setting_tableView.backgroundColor = [UIColor clearColor];
+    self.view.backgroundColor = MAIN_VIEW_COLOR;
+    self.rmm_setting_dataSource = @[@"清除缓存",@"版本更新",@"意见反馈"];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
 }
-*/
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.rmm_setting_dataSource.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:[NSString stringWithFormat:@"RMMSettingViewController-%ld",indexPath.row]];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    cell.backgroundColor = [UIColor clearColor];
+    if (indexPath.row < self.rmm_setting_dataSource.count) {
+        NSString *title = self.rmm_setting_dataSource[indexPath.row];
+        cell.textLabel.text = title;
+    }
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (indexPath.row == 0) {
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"清理成功"
+                                                                                 message:@""
+                                                                          preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *sureAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        }];
+        [alertController addAction:sureAction];
+        [self presentViewController:alertController animated:YES completion:nil];
+    }
+    else if (indexPath.row == 1){
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"当前已是最新版本"
+                                                                                 message:@""
+                                                                          preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *sureAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        }];
+        [alertController addAction:sureAction];
+        [self presentViewController:alertController animated:YES completion:nil];
+    }
+    else if (indexPath.row == 2){
+        RMMFeedbackViewController *vc = [RMMFeedbackViewController new];
+        vc.hidesBottomBarWhenPushed = YES;
+        vc.view.backgroundColor = MAIN_VIEW_COLOR;
+        [self.navigationController pushViewController:vc animated:YES];
+    }
+}
+
+#pragma mark - Lazy
+- (UITableView *)rmm_setting_tableView{
+    if (!_rmm_setting_tableView) {
+        _rmm_setting_tableView = [[UITableView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+        _rmm_setting_tableView.delegate = self;
+        _rmm_setting_tableView.dataSource = self;
+        _rmm_setting_tableView.rowHeight = 44;
+        _rmm_setting_tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    }
+    return _rmm_setting_tableView;
+}
 
 @end
